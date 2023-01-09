fix normalization, change gru training and add notebooks

experiments/configs.py
@@ -41,7 +41,7 @@ test = {
 
 grid_n_points_Y = {
         'niter': 1,
-        'name': 'test',
+        'name': 'grid_n_points_Y_lasso_no_norm',
         'data_config': {
             'model_X': 'cubic',
             'model_Y': 'cde',
@@ -50,27 +50,26 @@ grid_n_points_Y = {
             'n_val': 50,
             'n_points_true': 1000,
             'dim_X': 3,
-            'dim_Y': 2,
+            'dim_Y': 1,
             'non_linearity_Y': 'Tanh',
         },
         'exp_config': {
             'n_points_X': [100], #TODO: check somewhere that n_points_X > n_points_true +2
             'n_points_Y': [0, 1, 2, 3, 4, 5],
-            'model_names': [['ncde', 'lasso', 'gru']],
+            'model_names': [['lasso']],
             'model_hyperparams': [{
                 'lasso': {
-                    'alpha_grid': 10 ** np.linspace(-5, 5, 50),
                     'sig_order': [1, 2, 3, 4, 5],
-                    'normalize': True,
+                    'normalize': False,
                     'weighted': True,
                 },
                 'ncde': {
                     'vector_field': 'original',
-                    'num_epochs': 25,
+                    'num_epochs': 30,
                     'lr': 1e-3},
                 'gru': {
                     'gru_width': 128,
-                    'num_epochs': 50,
+                    'num_epochs': 100,
                     'lr': 1e-3},
                 }]
             }

src/models.py
@@ -30,8 +30,7 @@ warnings.simplefilter("ignore", category=ConvergenceWarning)
 
 class SigLasso:
     def __init__(self, sig_order: int, dim_Y, max_iter=1e3,
-                 normalize: bool = True, weighted: bool = False,
-                 alpha_grid: np.ndarray = 10 ** np.linspace(-7, 1, 50)):
+                 normalize: bool = True, weighted: bool = False):
         """
         Parameters
         ----------
@@ -45,16 +44,13 @@ class SigLasso:
         penalization.
         """
         self.sig_order = sig_order
-        self.alpha_grid = alpha_grid
-
         self.weighted = weighted
 
         self.dim_Y = dim_Y
         if self.dim_Y == 1:
-            self.reg = LassoCV(alphas=self.alpha_grid, max_iter=int(max_iter))
+            self.reg = LassoCV(max_iter=int(max_iter))
         else:
-            self.reg = MultiTaskLassoCV(alphas=self.alpha_grid,
-                                        max_iter=int(max_iter))
+            self.reg = MultiTaskLassoCV(max_iter=int(max_iter))
         self.normalize = normalize
 
     def train(self, X: torch.Tensor, Y: torch.Tensor,
@@ -66,10 +62,6 @@ class SigLasso:
                 "X must have 3 dimensions: n_samples, time, channels"
             assert Y.ndim == 3, \
                 "Y must have 3 dimensions: n_samples, time, channels"
-
-            # TODO : problem in normalization: do we do it here or for each subpath ?
-            if self.normalize:
-                X = normalize_path(X)
 
             sigX, Yfinal = self.get_final_matrices(X, Y, grid_Y=grid_Y)
 
@@ -83,6 +75,11 @@ class SigLasso:
     def get_final_matrices(
             self, X: torch.Tensor, Y: torch.Tensor, grid_Y: torch.Tensor = None
     ) -> Tuple[torch.Tensor, torch.Tensor]:
+
+        # TODO : problem in normalization: do we do it here or for each subpath ?
+        if self.normalize:
+            X = normalize_path(X)
+
         if Y.shape[1] == 1:
             return isig.sig(X, self.sig_order), Y[:, 0, :]
 

src/train.py
@@ -1,8 +1,7 @@
 import torch
 
 from src.models import GRUModel
-from src.utils import split_XY_on_grid, split_and_fill_XY_on_grid
-
+from src.utils import split_and_fill_XY_on_grid
 
 def train_gru(
         model: GRUModel,
@@ -10,24 +9,24 @@ def train_gru(
         Y_raw: torch.Tensor,
         num_epochs: int,
         lr: float = 0.001,
+        batch_size: int = 32,
         grid_Y: torch.Tensor = None):
 
-    # Split the paths in case Y is observed at several time points
-    X, Y = split_XY_on_grid(X_raw, Y_raw, grid_Y=grid_Y)
+    # Split the paths in case Y is observed at several time points and forward
+    # fill if irregular sampling of X
+    X, Y = split_and_fill_XY_on_grid(X_raw, Y_raw, grid_Y=grid_Y)
 
     optimizer = torch.optim.Adam(model.parameters(), lr=lr)
 
-    # train_dataset = torch.utils.data.IterableDataset(X, Y)
-    # train_dataloader = torch.utils.data.DataLoader(
-    #   train_dataset, batch_size=batch_size)
+    train_dataset = torch.utils.data.TensorDataset(X, Y)
+    train_dataloader = torch.utils.data.DataLoader(
+        train_dataset, batch_size=batch_size)
 
     for epoch in range(num_epochs):
-        # for batch in train_dataloader:
-        for i in range(len(X)): # Custom training loop to allow variable length sequences
-            batch_X = X[i].unsqueeze(0)
-            batch_y = Y[i].unsqueeze(0)
-
+        for batch in train_dataloader:
+            batch_X, batch_y = batch
             pred_y = model(batch_X)
+
             loss = torch.nn.MSELoss()(pred_y, batch_y)
             loss.backward()
             optimizer.step()

src/utils.py
@@ -157,7 +157,7 @@ def get_weights(dim_X,order):
     weight_vector = np.ones(isig.siglength(dim_X, order))
     position = 1
     for j in range(1, order+1):
-        factor = math.sqrt(j) / math.factorial(j)
+        factor = math.factorial(j) / math.sqrt(j)
         weight_vector[position:position + dim_X ** j] *= factor
         position += dim_X ** j
     return weight_vector
@@ -207,23 +207,24 @@ def matrix_to_function(X, time, interpolation_method):
     #             X[i, :, :], t=time[i, :])
 
 
-def normalize_path(df):
+def normalize_path(X):
     """
-    Normalizes the paths contained in df by their total variation norm.
+    Normalizes the paths contained in X by their total variation norm.
     Parameters
     ----------
-    df: dataframe of paths.
+    X: dataframe of paths.
 
     Returns
     -------
     Normalized dataframe of paths.
     """
-    n_sample = df.shape[0]
-    df_copy = df.detach().clone()
-    tv_norm = torch.linalg.norm(df_copy[:, 1:, :] - df_copy[:, :-1, :], axis=1).sum(axis=1)
+    n_sample = X.shape[0]
+    X_copy = X.detach().clone()
+    tv_norm = torch.linalg.norm(
+        X_copy[:, 1:, :] - X_copy[:, :-1, :], axis=2).sum(axis=1)
     for i in np.arange(n_sample):
-        df_copy[i, :, :] *= 1/tv_norm[i]
-    return df_copy
+        X_copy[i, :, :] *= 1/ (0.9 * tv_norm[i])
+    return X_copy
 
 
 def add_noise(df,variance):
@@ -288,9 +289,9 @@ def recontruct_Y(reg, X, length_Y, order):
 
 def l2_distance(X, Y):
     """
-    X and Y must be of shape (n_samples, n_points, dim)
+    X and Y must be of shape (n_samples, n_points, channels)
 
-    constant, not piecewise linear as we usually do in the theorems -> check this.
+   #TODO: ça ne va pas du tout cette formule
 
     """
     return np.mean(np.linalg.norm((np.array(X) - np.array(Y)) ** 2, axis=2))