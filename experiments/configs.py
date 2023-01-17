5f542ae8bff2cdb9c6dd60525a022168e2f0dce6
afermanian <adelinefermanian@hotmail.fr>
2023-01-17 12:15:29 +0100

    add tumor experiment

--- experiments/configs.py
+++ experiments/configs.py
@@ -5,7 +5,7 @@
 
 test = {
         'niter': 1,
-        'name': 'test_3',
+        'name': 'test',
         'data_config': {
             'model_X': 'brownian',
             'model_Y': 'ornstein_uhlenbeck',
@@ -34,6 +34,43 @@ test = {
                 'gru': {
                     'gru_width': 128,
                     'num_epochs': 5,
+                    'lr': 1e-3},
+                }]
+            }
+        }
+
+
+tumor = {
+        'niter': 1,
+        'name': 'tumor',
+        'data_config': {
+            'model_X': 'cubic_positive',
+            'model_Y': 'tumor_growth',
+            'n_train': 50,
+            'n_test': 50,
+            'n_val': 50,
+            'n_points_true': 1000,
+            'dim_X': 2,
+            'dim_Y': None,
+            'non_linearity_Y': None,
+        },
+        'exp_config': {
+            'n_points_X': [100],
+            'n_points_Y': [4],
+            'model_names': [['lasso', 'ncde', 'gru']],
+            'model_hyperparams': [{
+                'lasso': {
+                    'sig_order': [1, 2, 3, 4, 5, 6, 7, 8, 9],
+                    'normalize': True,
+                    'weighted': True,
+                },
+                'ncde': {
+                    'vector_field': 'original',
+                    'num_epochs': 30,
+                    'lr': 1e-3},
+                'gru': {
+                    'gru_width': 128,
+                    'num_epochs': 100,
                     'lr': 1e-3},
                 }]
             }
